7cdb3f3ed0e766f20fbe59696768da135cb829a4
Oleh Shevchyk <oshevchyck@gmail.com>
2020-11-20 15:40:09 +0200

    Finished builder

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './BurgerIngredient.module.scss';
+
+class BurgerIngredient extends Component {
+    render() {
+        let ingredient = null;
+
+        if (this.props.type === 'bread-top') {
+            ingredient = (
+                <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}/>
+                    <div className={classes.Seeds2}/>
+                </div>
+            )
+        } else {
+            const className = this.props.type.split('-')
+                .map(el => el.charAt(0).toUpperCase() + el.slice(1))
+                .join('');
+            ingredient = <div className={classes[className]}/>;
+        }
+
+        // switch (this.props.type) {
+        //     case ('bread-bottom'):
+        //         ingredient = <div className={classes.BreadBottom}/>;
+        //         break;
+        //     case ('bread-top'):
+        //         ingredient = (
+        //             <div className={classes.BreadTop}>
+        //                 <div className={classes.Seeds1}/>
+        //                 <div className={classes.Seeds2}/>
+        //             </div>
+        //         )
+        //         break
+        //     case ('meat'):
+        //         ingredient = <div className={classes.Meat}/>
+        //         break;
+        //     case ('cheese'):
+        //         ingredient = <div className={classes.Cheese}/>
+        //         break;
+        //     case ('salad'):
+        //         ingredient = <div className={classes.Salad}/>
+        //         break;
+        //     case ('bacon'):
+        //         ingredient = <div className={classes.Bacon}/>
+        //         break;
+        //     default: ingredient = null;
+        // }
+
+        return ingredient;
+    }
+}
+
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
+}
+
+export default BurgerIngredient;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import classes from './Layout.module.scss';
+
+const Layout = props => (
+    <Fragment>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Fragment>
+);
+
+export default Layout;
