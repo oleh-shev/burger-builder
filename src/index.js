df586323bf69e628e37b4a62ef9fbb06a738068e
Oleh Shevchyk <oshevchyck@gmail.com>
2020-12-08 23:13:07 +0200

    Refactor ordering to redux flow

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from "redux";
+import {createStore, applyMiddleware, compose, combineReducers} from "redux";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 
@@ -9,11 +9,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+import orderReducer from "./store/reducers/order";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer
+})
+
 const store = createStore(
-    burgerBuilderReducer,
+    rootReducer,
     composeEnhancers(
         applyMiddleware(thunk)
     )
