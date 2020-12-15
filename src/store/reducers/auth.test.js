96bac86338514f52be552bb2ddcd54f34a9b7c8d
Oleh Shevchyk <oshevchyck@gmail.com>
2020-12-15 23:25:54 +0200

    Add some simple tests

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,31 @@
+import reducer from "./auth";
+import * as actionTypes from "../actions/actionTypes";
+import {AUTH_SUCCESS} from "../actions/actionTypes";
+
+describe('auth reducer', () => {
+    const initialState = {
+        token: null,
+        userId: null,
+        error: null,
+        loading: false,
+        authRedirectPath: '/'
+    };
+
+    it('should return initial state', () => {
+        expect(reducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('should store the token upon login', () => {
+        expect(reducer(initialState, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: 'some-token',
+            userId: 'some-user-id'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+});
