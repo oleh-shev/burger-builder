5ea5684093bfe915cc757ade325fe3c2294fb20d
Oleh Shevchyk <oshevchyck@gmail.com>
2020-12-22 23:32:57 +0200

    Convert BurgerIngredient to func component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.module.scss';
 
-class BurgerIngredient extends Component {
-    render() {
-        let ingredient = null;
+const BurgerIngredient = props => {
+    let ingredient = null;
 
-        if (this.props.type === 'bread-top') {
-            ingredient = (
-                <div className={classes.BreadTop}>
-                    <div className={classes.Seeds1}/>
-                    <div className={classes.Seeds2}/>
-                </div>
-            )
-        } else {
-            const className = this.props.type.split('-')
-                .map(el => el.charAt(0).toUpperCase() + el.slice(1))
-                .join('');
-            ingredient = <div className={classes[className]}/>;
-        }
-        return ingredient;
+    if (props.type === 'bread-top') {
+        ingredient = (
+            <div className={classes.BreadTop}>
+                <div className={classes.Seeds1}/>
+                <div className={classes.Seeds2}/>
+            </div>
+        )
+    } else {
+        const className = props.type.split('-')
+            .map(el => el.charAt(0).toUpperCase() + el.slice(1))
+            .join('');
+        ingredient = <div className={classes[className]}/>;
     }
+    return ingredient;
 }
 
 BurgerIngredient.propTypes = {
